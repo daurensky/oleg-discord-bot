9969cd75241838dc34c883dfd9f07c32fb2c3e92
daurensky <dkambarov17@gmail.com>
2020-09-23 15:43:18 +0600

    added roll, flip and help commands

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const token = require("./token")
 
 const createWelcomeMessage = require("./lib/welcomeMessage")
 const addDefaultRole = require("./lib/defaultRole")
+const doRoll = require("./lib/doRoll")
+const doFlip = require("./lib/doFlip")
+const helpList = require("./lib/helpList")
 
 const client = new Discord.Client()
 
@@ -22,25 +25,53 @@ client.on("guildMemberAdd", async (member) => {
 })
 
 // COMMON MESSAGES
-client.on("message", (message) => {
-  const msg = message.content.toLowerCase()
+client.on("message", (msg) => {
+  const message = (text) => msg.content.toLowerCase() === text
 
-  if (msg === "хорошая работа олег") {
-    message.channel.send("<:ez:744200683363893398>")
+  if (message`пошел нахуй расул`) {
+    msg.channel.send("да, пошел нахуй")
+  }
+
+  if (message`хорошая работа олег`) {
+    msg.channel.send("<:ez:744200683363893398>")
   }
 })
 
-// TEST MESSAGES
-client.on("message", async (message) => {
-  const msg = message.content.toLowerCase()
-  const member = message.member
+// COMMANDS
+client.on("message", (msg) => {
+  const msgClear = msg.content.toLowerCase()
+  const sign = config.get("sign")
+  const command = (name) => msgClear === sign + name
+  const isRollCommand = msgClear.search(new RegExp(`${sign}roll`)) !== -1
 
-  if (msg === "test.hello") {
-    createWelcomeMessage(member, config.get("testChannel").toLowerCase())
+  if (msg.member.id !== config.get("bot_id")) {
+    // roll
+    if (isRollCommand) {
+      msg.channel.send(doRoll(msg))
+    }
+
+    // flip
+    if (command`flip`) {
+      msg.channel.send(doFlip(msg))
+    }
+
+    // help
+    if (command`help`) {
+      msg.channel.send(helpList())
+    }
+  }
+})
+
+// TEST MESSAGES (need to check action functions)
+client.on("message", async (msg) => {
+  const message = (text) => msg.content.toLowerCase() === text
+
+  if (message`test.hello`) {
+    createWelcomeMessage(msg.member, config.get("testChannel").toLowerCase())
   }
 
-  if (msg === "test.addrole") {
-    addDefaultRole(member)
+  if (message`test.addrole`) {
+    addDefaultRole(msg.member)
       .then(() => {
         message.channel.send("стандартная роль добавляется")
       })
