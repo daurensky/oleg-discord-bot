6444bf764fc2ceed84c98cabc2ac0dc8cb7aff1a
daurensky <dkambarov17@gmail.com>
2020-09-24 17:18:30 +0600

    updated text commands, help command and added enter to voice channel

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const addDefaultRole = require("./lib/defaultRole")
 const doRoll = require("./lib/doRoll")
 const doFlip = require("./lib/doFlip")
 const helpList = require("./lib/helpList")
+const TextCommand = require("./lib/textCommand")
 
 const client = new Discord.Client()
 
@@ -26,57 +27,93 @@ client.on("guildMemberAdd", async (member) => {
 
 // COMMON MESSAGES
 client.on("message", (msg) => {
-  const message = (text) => msg.content.toLowerCase() === text
+  const sended = new TextCommand(msg)
 
-  if (message("пошел нахуй расул")) {
-    msg.channel.send("да, пошел нахуй")
+  if (sended.message("хорошая работа олег")) {
+    msg.channel.send("<:ez:744200683363893398>")
   }
 
-  if (message("хорошая работа олег")) {
-    msg.channel.send("<:ez:744200683363893398>")
+  if (sended.inMessage("назым кто")) {
+    msg.channel.send("Назым чорт")
+  }
+
+  if (sended.inMessage("даурен гений")) {
+    msg.channel.send("<:DaurenGenius:758540102443728897>")
+  }
+
+  if (sended.message("пошел нахуй расул")) {
+    msg.channel.send("дада пошел нахуй")
   }
 })
 
 // COMMANDS
-client.on("message", (msg) => {
-  const msgClear = msg.content.toLowerCase()
-  const sign = config.get("sign")
-  const command = (name) => msgClear === sign + name
-  const isRollCommand = msgClear.search(new RegExp(`${sign}roll`)) !== -1
+client.on("message", async (msg) => {
+  const sended = new TextCommand(msg)
 
-  if (msg.member.id !== config.get("bot_id")) {
+  if (!sended.isBot) {
     // roll
-    if (isRollCommand) {
+    if (sended.inMessage(sended.sign + "roll")) {
       msg.channel.send(doRoll(msg))
     }
 
     // flip
-    if (command("flip")) {
+    if (sended.command("flip")) {
       msg.channel.send(doFlip(msg))
     }
 
     // help
-    if (command("help")) {
+    if (sended.command("help")) {
       msg.channel.send(helpList())
     }
+
+    // join
+    sended.voiceCommand("join", () => {
+      msg.channel.send("запрыгиваю в канал")
+      sended.voiceChannel.join()
+    })
+
+    // play
+    sended.voiceCommand("play", async () => {
+      const connection = await sended.voiceChannel.join()
+
+      const dispatcher = connection.play("audio.mp3")
+
+      dispatcher.on("start", () => {
+        msg.channel.send("включаю свою песню")
+      })
+
+      dispatcher.on("finish", () => {
+        msg.channel.send("конец песни")
+      })
+
+      dispatcher.setVolume(0.7)
+
+      dispatcher.on("error", console.error)
+    })
+
+    // leave
+    sended.voiceCommand("leave", () => {
+      msg.channel.send("выпрыгиваю из канала")
+      sended.voiceChannel.leave()
+    })
   }
 })
 
 // TEST MESSAGES (need to check action functions)
 client.on("message", async (msg) => {
-  const message = (text) => msg.content.toLowerCase() === text
+  const sended = new TextCommand(msg)
 
-  if (message("test.hello")) {
+  if (sended.test("hello")) {
     createWelcomeMessage(msg.member, config.get("testChannel").toLowerCase())
   }
 
-  if (message("test.addrole")) {
+  if (sended.test("addrole")) {
     addDefaultRole(msg.member)
       .then(() => {
-        message.channel.send("стандартная роль добавляется")
+        msg.channel.send("стандартная роль добавляется")
       })
       .catch(() => {
-        message.channel.send("неверное название роли в конфиге")
+        msg.channel.send("неверное название роли в конфиге")
       })
   }
 })
